023460f392ca9c5ea39a97f48e5f9ba5ad0b6cde
Mark Mysler <myslermark@gmail.com>
2024-12-23 20:21:24 -0300

    - searchInvitados.js

--- src/utils/searchInvitados.js
+++
@@ -1,9 +0,0 @@
-import { useGuestStore } from "../stores";
-
-const store = useGuestStore;
-
-export function searchInvitados(event, name) {
-	return store.invitados
-		.map((inv) => inv.fullname)
-		.filter((inv) => inv.toLowerCase().includes(name.toLowerCase()));
-}
